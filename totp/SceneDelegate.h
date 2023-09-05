e99718d0d6a3ae8f35521efce10ec7943292c78e
Fang Lu <cc2lufang@gmail.com>
2023-09-05 13:24:14 +0800

    Initial Commit

---
+++ totp/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  totp
+//
+//  Created by Fang Lu on 9/5/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ totp/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  totp
+//
+//  Created by Fang Lu on 9/5/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
