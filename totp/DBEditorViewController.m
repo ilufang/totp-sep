01a585be64a7e42d77b042b49971cb2e437667b9
Fang Lu <cc2lufang@gmail.com>
2025-10-16 02:23:37 +0900

    Fix layout

--- totp/DBEditorViewController.m
+++ totp/DBEditorViewController.m
@@ -20,6 +20,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 	_editor.text = _host.db;
+	NSLayoutConstraint *kbConstraint = [self.view.keyboardLayoutGuide.topAnchor constraintEqualToSystemSpacingBelowAnchor:_editor.bottomAnchor multiplier:1];
+	kbConstraint.active = true;
 }
 
 - (NSString *)db {
